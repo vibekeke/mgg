5e3b9592f76acc748ad5899f26f3c635d56b8b20
tyler <tyler.scott.mcallister@schibsted.com>
2022-06-14 20:16:32 +0200

    use seconds before, not a percentage

--- ActionLevels/backgrounds/TestBackground.gd
+++ ActionLevels/backgrounds/TestBackground.gd
@@ -6,23 +6,29 @@ export (float) var scrolling_speed_middleground = 500.0
 export (float) var scrolling_time_middleground = 10.0
 
 export (float) var scrolling_speed_foreground = 500.0
-export (float) var scrolling_time_foreground = 10.0
+export (float) var scrolling_time_foreground = 20.0
 
 export (float) var scrolling_speed_ending = 500.0
 export (float) var scrolling_time_ending = 10.0
 
-var basic_timer = 0
+var basic_timer = 0.0
 var foreground_stopped = false
 var timer_been_set = false
 
 func _physics_process(delta):
 	basic_timer += delta
+#	print('basic timer', basic_timer)
+#	print('scrolling time foreground', scrolling_time_foreground)
 	if basic_timer < scrolling_time_middleground:
 		$Middleground.scroll_offset.x -= scrolling_speed_middleground * delta
 	if basic_timer < scrolling_time_foreground:
 		$Foreground.scroll_offset.x -= scrolling_speed_foreground * delta
 	else:
 		foreground_stopped = true
-	if basic_timer / scrolling_time_foreground >= 0.65 and foreground_stopped == false:
+	if basic_timer / scrolling_time_middleground >= 0.3:
+		$BackgroundDude.position.x -= scrolling_speed_middleground * 10 * delta
+	if (scrolling_time_foreground - basic_timer) <= 3.5 and foreground_stopped == false:
+#		print($Ending/ParallaxLayer/Sprite.position)
+#		print($Ending/ParallaxLayer.position)
 		$Ending/ParallaxLayer/Sprite.visible = true
 		$Ending.scroll_offset.x -= scrolling_speed_ending * delta
