02c656fe213f2f59e3455863dca18606c47a48a0
tyler <tyler.scott.mcallister@schibsted.com>
2022-06-18 11:39:51 +0200

    level orchestrator spawns goofy platforms

--- ActionLevels/LevelCreator/LevelOrchestrator.gd
+++ ActionLevels/LevelCreator/LevelOrchestrator.gd
@@ -1,7 +1,18 @@
 extends Node2D
 
 export (String) var level_name
+export (float) var obstacle_spawn_freq
+onready var rng = RandomNumberGenerator.new()
+onready var timer = Timer.new()
 
 func _ready(): 
-	$ObstacleSpawner.spawn_level(level_name)
-	pass
+	rng.randomize()
+	timer.connect("timeout", self, "_on_call_obstacle_spawner")
+	timer.set_wait_time(obstacle_spawn_freq)
+	add_child(timer)
+	timer.start()
+
+func _on_call_obstacle_spawner():
+	$ObstacleSpawner.instance_default_for_level(level_name)
+	var obstacle_spawn_fuzziness = rng.randf_range(-0.5, 0.5)
+	timer.set_wait_time(obstacle_spawn_freq + obstacle_spawn_fuzziness)
--- ActionLevels/LevelCreator/Obstacles/ObstacleSpawner.gd
+++ ActionLevels/LevelCreator/Obstacles/ObstacleSpawner.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 var basic_floating_platform = preload("res://ActionLevels/LevelCreator/Obstacles/FloatingPlatform.tscn")	
+var total_instanced_obstacles = 0
+export (int) var default_obstacle_speed
 
-func spawn_level(level_name):
-	if level_name == "Level1":
-		var basic_floating_platform_node_1 = basic_floating_platform.instance()
-		basic_floating_platform_node_1.scroll_speed = 500
-		var basic_floating_platform_node_2 = basic_floating_platform.instance()
-		basic_floating_platform_node_2.scroll_speed = 500
-		basic_floating_platform_node_2.position.x -= 500
-		add_child(basic_floating_platform_node_1)
-		add_child(basic_floating_platform_node_2)
 	
+func instance_default_for_level(level_name):
+	if level_name == "Level1":
+		print(get_viewport().size)
+		var basic_floating_platform_node = basic_floating_platform.instance()
+		basic_floating_platform_node.position.x = 1930 # do this smarterer later
+		basic_floating_platform_node.position.y = 900 # do this smarterer later
+		basic_floating_platform_node.scroll_speed = default_obstacle_speed
+		add_child(basic_floating_platform_node)
