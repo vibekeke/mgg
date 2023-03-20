ab52f0a7eacc932113f2900999096c46d82c9704
tyler <tytheguy95@gmail.com>
2023-03-20 20:23:43 +0100

    working back and forth conversations in chat app. improved font size all over also

--- ActionLevels/Room/ChatApp.gd
+++ ActionLevels/Room/ChatApp.gd
@@ -16,14 +16,20 @@ onready var typing_sounds = [get_node("%TypingSound") , get_node("%TypingSound2"
 onready var current_typing_sound = 0
 
 onready var _type_timer = Timer.new()
+onready var _counterpart_is_typing_timer = Timer.new()
 
 func _ready():
 	randomize()
+	
 	_type_timer.set_wait_time(.01)
+	_type_timer.set_one_shot(true)
 	self.add_child(_type_timer)
+	
+	_counterpart_is_typing_timer.set_wait_time(1.0)
+	_counterpart_is_typing_timer.set_one_shot(true)
+	self.add_child(_counterpart_is_typing_timer)
+	
 	typing_sounds.shuffle()
-	print("printing typing sound state ", typing_sounds)
-
 
 func give_current_script():
 	var loaded_dialog = File.new()
@@ -40,7 +46,7 @@ func give_current_script():
 func reply_then_send():
 	pass
 
-func type_then_send(text_to_type):
+func type_then_send(text_to_type, sender):
 	var typing_sound = typing_sounds[current_typing_sound]
 	typing_sound.play()
 	for character in text_to_type:
@@ -51,7 +57,19 @@ func type_then_send(text_to_type):
 	current_typing_sound += 1
 	if current_typing_sound >= typing_sounds.size() - 1:
 		current_typing_sound = 0
-	chat_box.create_player_panel(text_to_type)
+	if sender == PLAYER_IDENTIFIER:
+		chat_box.create_player_panel(text_to_type)
+		check_counterpart_typing()
+	else:
+		# do some kind of '...' is typing thing here with a random wait
+		chat_box.create_counterpart_panel(text_to_type)
+		
+func check_counterpart_typing():
+	if script_array[current_dialog_entry_number]['name'] != PLAYER_IDENTIFIER:
+		var text_to_type = script_array[current_dialog_entry_number + 1]['script']
+		_counterpart_is_typing_timer.start()
+		yield(_counterpart_is_typing_timer, "timeout")
+		chat_box.create_counterpart_panel(text_to_type)
 
 func _on_ChatApp_gui_input(event):
 	if event is InputEventMouseButton:
@@ -61,15 +79,13 @@ func _on_ChatApp_gui_input(event):
 			else:
 				print_debug("That did not work dummy")
 
-
 func _on_SendMessageButton_button_up():
 	if current_dialog_entry_number == script_entries:
 		print("finished reading script")
 		return
 	var current_script_entry = script_array[current_dialog_entry_number]
 	if current_script_entry['name'] == PLAYER_IDENTIFIER:
-		type_then_send(current_script_entry['script'])
-		#chat_sender_box.text = current_script_entry['script']
+		type_then_send(current_script_entry['script'], PLAYER_IDENTIFIER)
 		chat_send_button.disabled = true
 	else:
 		chat_send_button.disabled = false
--- ActionLevels/Room/ChatBox/ChatBox.gd
+++ ActionLevels/Room/ChatBox/ChatBox.gd
@@ -1,91 +1,19 @@
 extends Control
 
 const player_chat_panel = preload("res://ActionLevels/Room/ChatBox/SettableChatPanel.tscn")
+const receiver_chat_panel = preload("res://ActionLevels/Room/ChatBox/SettableChatPanelCounterpart.tscn")
 
 func create_player_panel(text_for_panel):
 	var _instanced_player_chat_panel = player_chat_panel.instance()
 	_instanced_player_chat_panel.add_message_text(text_for_panel)
 	$ScrollContainer/VBoxContainer.add_child(_instanced_player_chat_panel)
+	yield(get_tree(), "idle_frame")
+	$ScrollContainer.ensure_control_visible(_instanced_player_chat_panel)
 
-func create_friend_panel(text_for_panel):
-	pass
-
-#onready var chat_app = get_node("%ChatApp")
-#onready var chatLog = get_node("VBoxContainer/RichTextLabel")
-#onready var inputLabel = get_node("VBoxContainer/HBoxContainer/Label")
-#onready var inputField = get_node("VBoxContainer/HBoxContainer/LineEdit")
-#
-#var groups = [
-#	{'name': 'Team', 'color': '#00abc7'},
-#	{'name': 'Match', 'color': '#ffdd8b'},
-#	{'name': 'Global', 'color': '#ffffff'}
-#]
-#var group_index = 0
-#var user_name = 'Player'
-#var script_place = 0
-
-#var conversationScript = [
-#	{ 'name': 'MGG', 'color': '#000000', 'script': 'Hello :D\n', 'delay': 1000 },
-#	{ 'name': 'Not an alien', 'color': '#000000', 'script': 'Hello there fellow human.\n', 'delay': 1000 },
-#	{ 'name': 'MGG', 'color': '#000000', 'script': 'I\'ll kill ya :)\n', 'delay': 1000 }
-#]
-
-#func _ready():
-#	inputField.connect("text_entered", self,'text_entered')
-#	change_group(0)
-
-#func _input(event):
-#	if event is InputEventKey:
-#		if event.pressed and event.scancode == KEY_ENTER:
-#			# temp
-#			pass
-#			if script_place >= conversationScript.size():
-#				print("Convo finished")
-#			else:
-#				var dialog_line = conversationScript[script_place]['script']
-#				var char_name = conversationScript[script_place]['name']
-#				var char_color = conversationScript[script_place]['color']
-#				var delay_ms = 0
-#				if conversationScript[script_place]['delay']:
-#					delay_ms = conversationScript[script_place]['delay']
-#				var delay_timer = Timer.new()
-#				delay_timer.set_wait_time(float(delay_ms) / 1000.0)
-#				add_child(delay_timer)
-#				delay_timer.start()
-#				yield(delay_timer, "timeout")
-#				message_entered(char_name, dialog_line, char_color)
-#				script_place += 1
-				
-#func message_entered(char_name, text, color):
-#	add_message(char_name, text, 0, color)
-#	inputField.text = ''
-#
-#func change_group(value):
-#	group_index += value
-#	if group_index > (groups.size() - 1):
-#		group_index = 0
-#	inputLabel.text = '[' + groups[group_index]['name'] + ']'
-#	inputLabel.set("custom_colors/font_color", Color(groups[group_index]['color']))
-#
-#func add_message(username, text, group = 0, color = ''):
-#	chatLog.bbcode_text += '\n' 
-#	if color == '':
-#		chatLog.bbcode_text += '[color=' + groups[group]['color'] + ']'
-#	else:
-#		chatLog.bbcode_text += '[color=' + color + ']'
-#	if username != '':
-#		chatLog.bbcode_text += '[' + username + ']: '
-#	chatLog.bbcode_text += text
-#	chatLog.bbcode_text += '[/color]'
-#
-#
-#func text_entered(text):
-#	if text =='/h':
-#		add_message('', 'There is no help message yet!', 0, '#ff5757')
-#		inputField.text = ''		
-#		return
-#	if text != '':
-#		add_message(user_name, text, group_index)
-#		# Here you have to send the message to the server
-#		print(text)
-#		inputField.text = ''
+func create_counterpart_panel(text_for_panel):
+	var _instanced_counterpart_chat_panel = receiver_chat_panel.instance()
+	_instanced_counterpart_chat_panel.add_message_text(text_for_panel)
+	$ScrollContainer/VBoxContainer.add_child(_instanced_counterpart_chat_panel)
+	yield(get_tree(), "idle_frame")
+	$ScrollContainer.ensure_control_visible(_instanced_counterpart_chat_panel)
+	
