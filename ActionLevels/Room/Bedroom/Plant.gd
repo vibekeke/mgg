989066cb1501303c305da88863d23c6a583d6ea7
tyler <tytheguy95@gmail.com>
2023-12-24 21:40:33 +0000

    xmas commit 🎄

--- ActionLevels/Room/Bedroom/Plant.gd
+++ ActionLevels/Room/Bedroom/Plant.gd
@@ -3,7 +3,7 @@ extends Sprite
 var player_in_area : bool = false
 var dialogue_open : bool = false
 
-onready var dialog_box = get_node("%DialogBox")
+onready var dialog_box = get_node("%TestDialogueCreator")
 
 func _ready():
 	dialog_box.connect("dialogue_box_finished", self, "_on_dialogue_box_finished")
@@ -17,12 +17,14 @@ func _on_InteractableArea_body_exited(body):
 		player_in_area = false
 
 func display_dialogue():
-	dialog_box.create_dialogue_bubble()
+	dialog_box.create_dialogue_balloon()
 	dialogue_open = true
 	
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept") and player_in_area and !dialogue_open:
+		print("display dialogue")
 		display_dialogue()
 
 func _on_dialogue_box_finished():
+	print("dialogue box finished")
 	dialogue_open = false
