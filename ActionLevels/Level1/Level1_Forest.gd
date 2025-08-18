8ec4317ccf763c4e0871d922dd768c4085f7c371
Tyler <tyler.mcallister@aidn.no>
2025-08-18 19:27:24 +0200

    Add level start display for displaying tutorial to the user before the level begins

--- ActionLevels/Level1/Level1_Forest.gd
+++ ActionLevels/Level1/Level1_Forest.gd
@@ -7,15 +7,14 @@ onready var background_music = $BackgroundMusic
 onready var boss_music = $BossMusic
 onready var level_background = get_node("%LevelBackground")
 onready var enemy_spawner = get_node("%EnemySpawner")
+onready var level_start_display = get_node("%LevelStartDisplay")
 export var boss_background : PackedScene
-export var legacy_version : bool = false
 export var mute_audio = false # temporary
 
 func _ready():
+	level_start_display.connect("confirm_level_start", self, "_on_confirm_level_start")
 	enemy_spawner.stop_enemy_spawner()
 	Events.connect("boss_spawned", self, "_on_boss_spawn")
-	# if the level is the main scene with no valid parent and legacy flag is enabled run the old code
-	print(self.get_parent().name)
 	
 	Events.emit_signal("background_moving_enabled", false)
 	Events.emit_signal("player_standing", true)
@@ -28,7 +27,6 @@ func _ready():
 		spawn_point_array.append(spawn_paths.get_curve().get_point_position(x))
 		spawn_point_dictionary[spawn_point_heights[x]] = spawn_paths.get_curve().get_point_position(x)
 	Events.emit_signal("level_spawn_points", spawn_point_dictionary)
-	add_initial_background_element()
 	if !mute_audio:
 		if !background_music.is_playing():
 			background_music.play()
@@ -44,10 +42,10 @@ func _on_boss_spawn():
 		background_music.stop()
 		boss_music.play()
 
-
-func _on_StartLevelTimer_timeout():
+func _on_confirm_level_start():
 	enemy_spawner.start_enemy_spawner()
 	Events.emit_signal("background_moving_enabled", true)
 	Events.emit_signal("player_standing", false)
 	Events.emit_signal("fall_down_ui")
 	self.emit_signal("level_start")
+	add_initial_background_element()
