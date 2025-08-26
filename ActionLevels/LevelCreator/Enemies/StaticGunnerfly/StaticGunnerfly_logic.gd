48ccd99a7dd93ca4828634a190f5137260708c0a
Ty <tytheguy95@gmail.com>
2025-08-26 18:35:50 +0200

    give gunnerfly a better projectile (#40)

--- ActionLevels/LevelCreator/Enemies/StaticGunnerfly/StaticGunnerfly_logic.gd
+++ ActionLevels/LevelCreator/Enemies/StaticGunnerfly/StaticGunnerfly_logic.gd
@@ -25,11 +25,11 @@ var hit_end : bool = false
 
 const phase_patterns = {
 	0: {
-		'rotate_speed': 115, # speed of rotation of bullet generator
+		'rotate_speed': 0, # speed of rotation of bullet generator
 		'spawn_point_count': 1, # number of bullets to spawn at one time, equivalent of angles to complete a circle, e.g. 3 = 120 degrees at each point
-		'fire_rate_timer_wait_time': 0.5, # how often to fire in seconds
+		'fire_rate_timer_wait_time': 1.0, # how often to fire in seconds
 		'radius': 1, # size of radius of bullet spawning, influences initial "closeness" of bullets
-		'projectile_speed': 100 # speed of the projectiles that are generated
+		'projectile_speed': -850 # speed of the projectiles that are generated
 	}
 }
 
@@ -93,7 +93,6 @@ func _setup_bullets():
 
 
 func apply_new_bullet_phase(phase_number: int):
-	print("transitioning to phase -> ", phase_number)
 	if debug_mode:
 		pass
 	else:
