f9687d3f35de0ffe3411c448c40e207f5c1dcd90
Tyler <tyler.mcallister@aidn.no>
2025-08-28 19:57:35 +0200

    Set death explosion to scroll the same speed as the enemy who created it

--- ActionLevels/LevelCreator/Enemies/EnemyAssets/AnimatedEnemyExplosion.gd
+++ ActionLevels/LevelCreator/Enemies/EnemyAssets/AnimatedEnemyExplosion.gd
@@ -1,9 +1,12 @@
 extends AnimatedSprite
 
+export var scroll_speed : float = 500.0
 
 func _ready():
 	pass
 
+func _process(delta):
+	self.position.x -= delta * 1.25 * scroll_speed
 
 func _on_AnimatedEnemyExplosion_animation_finished():
 	self.queue_free()
