735e32053346b0d830c25d2e594c60bb09910119
Vibeke Ihlen Selnes <107354551+vibekeke@users.noreply.github.com>
2025-09-02 14:34:38 +0200

    updated gameoverscreen + sounds

--- Menus/GameOver.gd
+++ Menus/GameOver.gd
@@ -17,6 +17,12 @@ onready var score_display : RichTextLabel = get_node("%ScoreDisplay")
 
 var player_final_score : int = 0
 
+
+onready var audio_player = $AudioStreamPlayer
+var hover_sfx = preload("res://sounds/UI sounds/vgmenuhover.wav")
+var confirm_sfx = preload("res://sounds/UI sounds/vgmenuselect.wav")
+var first_focus = true
+
 func _ready():
 	# Set initial invisible state
 	animated_sprite.modulate = Color(1, 1, 1, 0)
@@ -25,11 +31,15 @@ func _ready():
 	player_final_score = Events.get_score()
 	score_display.bbcode_text = score_display.bbcode_text + " " + str(player_final_score)
 	animation_player.play("fade_in")
+	audio_player.stream = hover_sfx
+	
 
 func _on_RetryButton_pressed():
 	if faded_in:
 		sad_sprite.hide()
 		determined_sprite.show()
+		audio_player.stream = confirm_sfx
+		audio_player.play()
 		yield(get_tree().create_timer(0.5), "timeout")
 		
 		Events.emit_signal("transition_to_scene", "Level1", false)
@@ -45,6 +55,10 @@ func _on_AnimationPlayer_animation_finished(anim_name):
 		retry_button.grab_focus()
 
 func _on_RetryButton_focus_entered():
+	if first_focus:
+		first_focus = false
+	else:
+		audio_player.play()
 	retry_star.visible = true
 
 
@@ -58,6 +72,7 @@ func _on_RetryButton_mouse_entered():
 
 
 func _on_QuitButton_focus_entered():
+	audio_player.play()
 	quit_star.visible = true
 	
 
