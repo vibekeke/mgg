1d907fe77949e70a427da966cc3d9599e6f839a1
Tyler <tyler.mcallister@aidn.no>
2025-09-08 19:44:50 +0200

    Sounds for enemy projects and OurGuy fix

--- ActionLevels/LevelCreator/Enemies/BroBear/BroBear.gd
+++ ActionLevels/LevelCreator/Enemies/BroBear/BroBear.gd
@@ -32,6 +32,7 @@ func launch_bullet(bullet: Object):
 	if player_position != null:
 		bullet.player_position = player_position
 		bullet.shoot_towards = true
+		AudioManager.playSFX("LaserBlipShort", 1.0, -20)
 	else:
 		print_debug("No player position found! Did you check the scene tree?")
 
