16ed562cdf341a3a5b46bb1ca6e08692a4b578c0
Tyler <tyler.mcallister@aidn.no>
2025-09-21 16:33:45 +0200

    Use tween instead of raw values for trauma

--- Cutscenes/ShakeyCam.gd
+++ Cutscenes/ShakeyCam.gd
@@ -1,15 +1,39 @@
 extends Node
 
-export var handcam_intensity: float = 2.0
-export var handcam_frequency: float = 2.0
+export var shake_intensity: float = 15.0
+export var rotation_intensity: float = 0.01
+export var shake_duration_min: float = 0.8
+export var shake_duration_max: float = 2.0
 
 var camera: Camera2D
-var time: float = 0.0
+var tween: Tween
+var base_position: Vector2
+var base_rotation: float
 
 func _ready():
 	camera = get_parent()
+	base_position = camera.position
+	base_rotation = camera.rotation
+	tween = Tween.new()
+	add_child(tween)
+	tween.connect("tween_all_completed", self, "_on_shake_complete")
+	start_next_shake()
 
-func _process(delta):
-	time += delta
-	var shake_amount = handcam_intensity * (0.5 + 0.5 * sin(time * handcam_frequency))
-	camera.add_trauma(shake_amount * delta)
+func start_next_shake():
+	var duration = rand_range(shake_duration_min, shake_duration_max)
+	var target_offset = Vector2(
+		rand_range(-shake_intensity, shake_intensity),
+		rand_range(-shake_intensity, shake_intensity)
+	)
+	var target_rotation = base_rotation + rand_range(-rotation_intensity, rotation_intensity)
+
+	tween.interpolate_property(camera, "position",
+		camera.position, base_position + target_offset,
+		duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.interpolate_property(camera, "rotation",
+		camera.rotation, target_rotation,
+		duration, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+	tween.start()
+
+func _on_shake_complete():
+	start_next_shake()
