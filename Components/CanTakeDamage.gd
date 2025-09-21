31a688b05c32317a5c72460c1a90083d2087a434
Tyler <tyler.mcallister@aidn.no>
2025-09-21 23:34:09 +0200

    RETRY

--- Components/CanTakeDamage.gd
+++ Components/CanTakeDamage.gd
@@ -16,6 +16,7 @@ onready var death_explosion = load("res://ActionLevels/LevelCreator/Enemies/Enem
 export var health_value = 2
 
 onready var damage_timer = get_node("%DamageTimer")
+onready var off_screen_timer = get_node("%OffscreenTimer")
 
 export (Color) var hurt_colour = Color(10,10,10,1)
 
@@ -67,9 +68,7 @@ func _on_screen_entered():
 	damageable = true
 
 func _on_screen_exited():
-	damageable = false
-	if !death_called:
-		emit_signal("enemy_return_to_pool", enemy_node)
+	off_screen_timer.start()
 
 func _on_explosion_finished():
 	emit_signal("enemy_return_to_pool", enemy_node)
@@ -77,3 +76,8 @@ func _on_explosion_finished():
 func _on_damage_timer():
 	enemy_sprite_node.modulate = Color(1,1,1,1)
 	damage_timer.stop()
+	
+func _on_OffscreenTimer_timeout():
+	damageable = false
+	if !death_called:
+		emit_signal("enemy_return_to_pool", enemy_node)
