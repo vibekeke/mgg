e1d84fe75dcd7a6aa07e964df3e7d6025c8a0f16
Ty <tytheguy95@gmail.com>
2025-09-22 18:53:25 +0200

    fix dogs and add continue button focus to animation player (#130)

--- Autoload/Events.gd
+++ Autoload/Events.gd
@@ -101,7 +101,6 @@ func initialize():
 	first_time_playing = SaveFileManager.get_first_time_playing()
 	dogs_complete = SaveFileManager.get_all_dogs_collected()
 	is_initialized = true
-	print("Events: Loaded from save: first_time_playing = ", first_time_playing, ", dogs_complete = ", dogs_complete)
 
 func _disable_player_actions(to_disable: bool):
 	# for tutorial and other stuff, e.g. boss loading?
@@ -193,8 +192,8 @@ func save_game(level_name : int, dog_info : Dictionary):
 func update_dogs(dog_type: String):
 	if not COLLECTED_DOGS.has(dog_type):
 		COLLECTED_DOGS[dog_type] = true
+
 	if COLLECTED_DOGS.size() >= 3:
-		dogs_complete = true
 		SaveFileManager.set_all_dogs_collected(true)
 		self.emit_signal("collected_all_dogs")
 		
